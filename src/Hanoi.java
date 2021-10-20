84268174fad0bdf15bee3732fcc40a838bdd29b0
Alex@TKA <alextchoupou@gmail.com>
2021-10-20 01:41:55 +0100

    init commit

---
+++ src/Hanoi.java
@@ -0,0 +1,15 @@
+public class Hanoi {
+    public static void main(String[] args) {
+        HanoiTower(4, 'X', 'Z', 'Y');
+
+    }
+     static void  HanoiTower(int n, char depart, char intermediaire, char arrivee){
+        if(n==1){
+            System.out.println("Déplacer le disque"+n+" de la tige de depart:"+depart+" à l'arrivee:"+arrivee);
+        }else{
+
+        HanoiTower(n-1, depart,arrivee,intermediaire);
+        System.out.println("Déplacer le disque"+n+" de la tige de depart:"+depart+" à l'arrivee:"+arrivee);
+        HanoiTower(n-1, intermediaire,depart,arrivee);}
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int []tab={1,2,3,4,5,6,7,8,9};
+        sumProductTableau(tab);
+        pairTableau(tab);
+        System.out.println(Arrays.toString(tab));
+        System.out.println();
+        reverseTableau(tab);
+        Hanoi.HanoiTower(4, 'X', 'Z', 'Y');
+    }
+
+    static void sumProductTableau(int [] tableau){
+        int sum=0;
+        int product=1;
+
+         for (int j : tableau) {
+             sum = sum + j;
+             product = product * j;
+         }
+        System.out.println("La somme du tableau est "+sum+" Le produit du tableau est "+product);
+    }
+    static void pairTableau(int [] tableau){
+        for (int k : tableau) {
+            for (int i : tableau) {
+                System.out.println(k + "" + i);
+            }
+            System.out.println();
+        }
+    }
+    static void reverseTableau(int [] tableau){
+
+        for (int i=0;i<tableau.length/2;i++){
+
+            int temp= tableau[tableau.length-1-i];
+            tableau[tableau.length-1-i]= tableau[i];
+            tableau[i]=temp;
+
+        }
+        System.out.println(Arrays.toString(tableau));
+
+    }
+}
